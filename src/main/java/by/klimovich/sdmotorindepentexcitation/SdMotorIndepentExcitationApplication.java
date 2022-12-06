5575c34dcd0cf387f4b089873aadea2d974d5f71
Alexsei <alexseiklimovichdev@gmail.com>
2022-12-06 21:48:14 +0300

    Initial commit

---
+++ src/main/java/by/klimovich/sdmotorindepentexcitation/SdMotorIndepentExcitationApplication.java
@@ -0,0 +1,13 @@
+package by.klimovich.sdmotorindepentexcitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdMotorIndepentExcitationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdMotorIndepentExcitationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/klimovich/sdmotorindepentexcitation/SdMotorIndepentExcitationApplicationTests.java
@@ -0,0 +1,13 @@
+package by.klimovich.sdmotorindepentexcitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdMotorIndepentExcitationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
